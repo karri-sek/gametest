77140cdb2bfc98d0f0d540a882e23f32004d95ca
sekhar karri <babusek@gmail.com>
2017-02-18 22:46:03 +0000

    Adding Starter file for Spring boot application

---
+++ src/main/java/gamesys/Application.java
@@ -0,0 +1,17 @@
+package gamesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sekhar on 18/2/17.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
