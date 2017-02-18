4dc42dab759e3e625f54d89d5c2d4b0fb10346fa
sekhar karri <babusek@gmail.com>
2017-02-18 21:54:06 +0000

    Adding implementation code to pass the test written for check the existence of source file

--- src/main/java/gamesys/file/FileExercise.java
+++ src/main/java/gamesys/file/FileExercise.java
@@ -1,5 +1,13 @@
 package gamesys.file;
 
+import java.io.File;
+
 public class FileExercise {
+
+    public boolean checkIsInputFileExists(String path){
+        if(path == null || path.isEmpty()) return false;
+        File inputFile = new File(path);
+        return inputFile.exists();
+    }
 
 }
