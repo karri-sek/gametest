41861ce39947ec8950cb46ed22f861835e1a1ba3
sekhar karri <babusek@gmail.com>
2017-02-18 22:22:30 +0000

    Adding implementation to reverse the source file content into new destination file

--- src/main/java/gamesys/file/FileExercise.java
+++ src/main/java/gamesys/file/FileExercise.java
@@ -1,6 +1,13 @@
 package gamesys.file;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileExercise {
 
@@ -8,6 +15,24 @@ public class FileExercise {
         if(path == null || path.isEmpty()) return false;
         File inputFile = new File(path);
         return inputFile.exists();
+    }
+
+    public void reverseAllWordsInFile(String path) throws IOException {
+        String reversed = Files.readAllLines(Paths.get(path)).stream()
+                .flatMap(line -> Arrays.asList(line.split(" +")).stream())
+                .map(word -> new StringBuilder(word).reverse())
+                .collect(Collectors.joining(" "));
+        List<String> listOfReversedWords = Arrays.asList(reversed.split(" "));
+        Collections.reverse(listOfReversedWords);
+        Files.write(Paths.get(prepareReversedFileName(path)),String.join(" ", listOfReversedWords).getBytes());
+    }
+
+    private String prepareReversedFileName(String path) {
+        int index = path.indexOf(".");
+        String sourceFileNameWithoutExt = path.substring(0,index);
+        String sourceFileExt = path.substring(index);
+        StringBuffer sb = new StringBuffer(sourceFileNameWithoutExt).append("_reversed").append(sourceFileExt);
+        return sb.toString();
     }
 
 }
