fac848f780667fa139f6b58bb7608b80f5c0619a
sekhar karri <babusek@gmail.com>
2017-02-18 22:07:07 +0000

    Adding test case to revere the input file content

--- src/test/java/gamesys/interview/test/file/FileExerciseTest.java
+++ src/test/java/gamesys/interview/test/file/FileExerciseTest.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class FileExerciseTest {
 
@@ -29,6 +32,16 @@ public class FileExerciseTest {
     public void shouldReturnFalseWhenInValidFilePath() {
         String path = "src/main/resources/META-INF/samplefile.txt";		;
         Assert.assertEquals(false, fileExercise.checkIsInputFileExists(path));
+    }
+
+    @Test
+    public void shouldCreateDestinationFileAndContentShouldbeReversed() throws IOException{
+        String path = "src/main/resources/META-INF/files/samplefile.txt";
+        fileExercise.reverseAllWordsInFile(path);
+
+        String destFile = "src/main/resources/META-INF/files/samplefile_reversed.txt";
+        List<String> s = Files.readAllLines(Paths.get(destFile));
+        Assert.assertEquals("desrever eb ot sdrow elpmas emos sniatnoc elif sihT", s.get(0));
     }
 
 	@Test
