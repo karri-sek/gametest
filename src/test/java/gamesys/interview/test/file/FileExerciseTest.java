a58b61e6e33b5dabd92030343147a5b1ad229efd
sekhar karri <babusek@gmail.com>
2017-02-18 21:50:02 +0000

    Adding junit setup for File Exercise

--- src/test/java/gamesys/interview/test/file/FileExerciseTest.java
+++ src/test/java/gamesys/interview/test/file/FileExerciseTest.java
@@ -1,8 +1,21 @@
 package gamesys.interview.test.file;
 
+import gamesys.file.FileExercise;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class FileExerciseTest {
+
+    FileExercise fileExercise = null;
+
+	@Before
+	public void setUp() throws IOException, URISyntaxException {
+		fileExercise = new FileExercise();
+	}
 
 	@Test
 	public void testFileExist() {
@@ -13,5 +26,10 @@ public class FileExerciseTest {
 	public void testFileCDontentReversed() {
 
 	}
+
+    @After
+    public void tearDown() {
+        fileExercise = null;
+    }
 
 }
