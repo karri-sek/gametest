b41091ab02d893f66e25c2b419c42b2ca87418a5
sekhar karri <babusek@gmail.com>
2017-02-19 22:38:29 +0000

    Adding Get End point to calculate the Average value of Guitar type using spring boot Get Point

--- src/main/java/gamesys/guitarshop/GuitarShop.java
+++ src/main/java/gamesys/guitarshop/GuitarShop.java
@@ -1,6 +1,13 @@
 package gamesys.guitarshop;
 
+import gamesys.model.Guitar;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -14,6 +21,20 @@ public class GuitarShop {
 				.filter(line -> type.equalsIgnoreCase(line.type) || type.isEmpty())
 				.collect(Collectors.averagingDouble(line -> line.cost));
 		return average;
+	}
+
+	@RequestMapping(value="/get-average-price",method = RequestMethod.GET)
+	public @ResponseBody
+	Guitar getAvgPriceByType(@RequestParam(value="type",defaultValue = "classic") String type) throws IOException, URISyntaxException {
+
+		Guitar guitar = new Guitar();
+		List<String[]> data = getData();
+		double average = data.stream().map(arr -> new DataLine(arr[2], Double.parseDouble(arr[3])))
+				.filter(line -> type.equalsIgnoreCase(line.type) || type.isEmpty())
+				.collect(Collectors.averagingDouble(line -> line.cost));
+		guitar.setType(type);
+		guitar.setAveragePrice(average);
+		return guitar;
 	}
 
 	private List<String[]> getData() throws IOException {
---
+++ src/main/java/gamesys/model/Guitar.java
@@ -0,0 +1,21 @@
+package gamesys.model;
+
+public class Guitar {
+	
+	private String type;
+	private double averagePrice;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public double getAveragePrice() {
+		return averagePrice;
+	}
+	public void setAveragePrice(double averagePrice) {
+		this.averagePrice = averagePrice;
+	}
+
+}
